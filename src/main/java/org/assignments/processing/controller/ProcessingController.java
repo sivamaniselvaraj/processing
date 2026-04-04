bc9ad3da97e7658bc18f136fe8217574428bd331
sivamani selvaraj <talk2sivamani@gmail.com>
2026-04-04 22:00:38 +0530

    implemented open api

--- src/main/java/org/assignments/processing/controller/ProcessingController.java
+++ src/main/java/org/assignments/processing/controller/ProcessingController.java
@@ -1,11 +1,14 @@
 package org.assignments.processing.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
 import org.assignments.processing.service.ProcessingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 @RequestMapping("/processing")
 public class ProcessingController {
 
@@ -13,12 +16,13 @@ public class ProcessingController {
     ProcessingService processingService;
 
     @PutMapping("/{orderId}/status")
-    public ResponseEntity updateStatus(
+    @Operation(summary = "Update order status")
+    public ResponseEntity<?> updateStatus(
             @PathVariable Long orderId,
             @RequestParam String status){
-
+        log.info("Received update status request for: {} status: {}", orderId, status);
         processingService.updateStatus(orderId,status);
-
-        return ResponseEntity.ok("updated");
+        log.info("update status successfully for: {}", orderId );
+        return ResponseEntity.ok("update status successfully for: " + orderId + "to: " + status);
     }
 }
